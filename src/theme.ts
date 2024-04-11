12e8530913b7d5ba2f501d8e96f80192efb61e94
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-04-11 19:41:57 +0900

    #5.0 Setup

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,7 @@
 import { DefaultTheme } from "styled-components/dist/types";
 
-export const lightTheme: DefaultTheme = {
+export const theme: DefaultTheme = {
   bgColor: "white",
   textColor: "black",
   btnColor: "tomato",
-};
-
-export const darkTheme: DefaultTheme = {
-  bgColor: "black",
-  textColor: "white",
-  btnColor: "teal",
 };
