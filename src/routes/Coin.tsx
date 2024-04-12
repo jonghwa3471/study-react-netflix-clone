31a7ed2abfc27dcbc1ffa2472ec0e4e94056b1fc
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-04-12 22:08:54 +0900

    #5.4 Route States

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,50 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 interface RouteParams {
   coinId: string;
 }
 
+interface RouteState {
+  name: string;
+}
+
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
-  return <h1>Coin: {coinId}</h1>;
+  const { state } = useLocation<RouteState>();
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading.."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
