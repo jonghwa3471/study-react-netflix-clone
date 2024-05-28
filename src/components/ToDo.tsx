5b630438a90b7b6d2b1958b18917f132a70a5625
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-05-28 22:01:42 +0900

    #6.12 Refactoring

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,14 @@
+import { IToDo } from "../atoms";
+
+function ToDo({ text }: IToDo) {
+  return (
+    <li>
+      <span>{text}</span>
+      <button>To Do</button>
+      <button>Doing</button>
+      <button>Done</button>
+    </li>
+  );
+}
+
+export default ToDo;
