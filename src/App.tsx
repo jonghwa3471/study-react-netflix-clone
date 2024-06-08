e148e8f162bbe37f44dadd127f7966a7aa588cff
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-06-08 17:51:31 +0900

    #7.3 Drag and Drop part Two

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,15 @@ function App() {
     <DragDropContext onDragEnd={onDragEnd}>
       <div>
         <Droppable droppableId="one">
-          {() => (
-            <ul>
+          {(provided) => (
+            <ul ref={provided.innerRef} {...provided.droppableProps}>
               <Draggable draggableId="first" index={0}>
-                {() => <li>One</li>}
-              </Draggable>
-              <Draggable draggableId="second" index={1}>
-                {() => <li>Two</li>}
+                {(provided) => (
+                  <li ref={provided.innerRef} {...provided.draggableProps}>
+                    <span {...provided.dragHandleProps}>🔥</span>
+                    One
+                  </li>
+                )}
               </Draggable>
             </ul>
           )}
