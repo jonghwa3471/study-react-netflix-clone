2661bafea4f63ca2a86e3360b4e0687f62318c33
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-07-02 20:57:35 +0900

    #7.13 Refs

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,7 @@
 import { Droppable } from "react-beautiful-dnd";
 import DraggableCard from "./DraggableCard";
 import styled from "styled-components";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   width: 300px;
@@ -41,9 +42,18 @@ interface IBoardProps {
 }
 
 function Board({ toDos, boardId }: IBoardProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClick = () => {
+    inputRef.current?.focus();
+    setTimeout(() => {
+      inputRef.current?.blur();
+    }, 3000);
+  };
   return (
     <Wrapper>
       <Title>{boardId}</Title>
+      <input ref={inputRef} placeholder="grab me"></input>
+      <button onClick={onClick}>click me</button>
       <Droppable droppableId={boardId}>
         {(provided, snapshot) => (
           <Area
