2a870ca2e3d15cf4295dd01fba76b714bda398f3
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-07-19 16:38:17 +0900

    #8.0 Introduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,126 +1,26 @@
-import { DragDropContext, DropResult } from "react-beautiful-dnd";
-import { useRecoilState, useRecoilValue } from "recoil";
 import styled from "styled-components";
-import {
-  boardModalState,
-  boardOrderState,
-  editModalState,
-  toDoState,
-  TRELLO_TODO,
-} from "./atoms";
-import Board from "./components/Board";
-import AddBoardButton from "./components/AddBoardButton";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import BoardModal from "./components/BoardModal";
-import EditToDo from "./components/EditToDo";
-import DroppableTrashCan from "./components/DroppableTrashCan";
-import ClearButton from "./components/ClearButton";
-
-library.add(fas, fab, far);
 
 const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
   display: flex;
-  max-width: 1024px;
-  width: 100%;
-  min-height: 100vh;
-  margin: 0 auto;
   justify-content: center;
   align-items: center;
-  overflow-y: auto;
 `;
 
-const Boards = styled.div`
-  padding: 20px;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 100%;
-  gap: 20px;
-`;
-
-const TrashCan = styled.div`
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-  width: 150px;
-  height: 150px;
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
-  const boardOrder = useRecoilValue(boardOrderState);
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const modalState = useRecoilValue(boardModalState);
-  const editState = useRecoilValue(editModalState);
-  const onDragEnd = (info: DropResult) => {
-    const { destination, source } = info;
-    if (!destination) return;
-    if (destination?.droppableId === source.droppableId) {
-      // same board movement.
-      setToDos((allBoards) => {
-        const boardCopy = [...allBoards[source.droppableId]];
-        const taskObj = boardCopy[source.index];
-        boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination?.index, 0, taskObj);
-        const dragEndResult = {
-          ...allBoards,
-          [source.droppableId]: boardCopy,
-        };
-        localStorage.setItem(TRELLO_TODO, JSON.stringify(dragEndResult));
-        return dragEndResult;
-      });
-    }
-    if (destination.droppableId !== source.droppableId) {
-      if (destination.droppableId === "trashCan") {
-        setToDos((allBoards) => {
-          const copiedBoardToDos = [...allBoards[source.droppableId]];
-          copiedBoardToDos.splice(source.index, 1);
-          const result = {
-            ...allBoards,
-            [source.droppableId]: copiedBoardToDos,
-          };
-          localStorage.setItem(TRELLO_TODO, JSON.stringify(result));
-          return result;
-        });
-      } else {
-        // cross board movement.
-        setToDos((allBoards) => {
-          const sourceBoard = [...allBoards[source.droppableId]];
-          const taskObj = sourceBoard[source.index];
-          const destinationBoard = [...allBoards[destination.droppableId]];
-          sourceBoard.splice(source.index, 1);
-          destinationBoard.splice(destination.index, 0, taskObj);
-          const dragEndResult = {
-            ...allBoards,
-            [source.droppableId]: sourceBoard,
-            [destination.droppableId]: destinationBoard,
-          };
-          localStorage.setItem(TRELLO_TODO, JSON.stringify(dragEndResult));
-          return dragEndResult;
-        });
-      }
-    }
-  };
   return (
-    <>
-      <AddBoardButton />
-      <ClearButton />
-      {editState ? <EditToDo /> : null}
-      {modalState ? <BoardModal /> : null}
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Wrapper>
-          <Boards>
-            {boardOrder.map((boardId) => (
-              <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
-            ))}
-          </Boards>
-          <TrashCan>
-            <DroppableTrashCan></DroppableTrashCan>
-          </TrashCan>
-        </Wrapper>
-      </DragDropContext>
-    </>
+    <Wrapper>
+      <Box />
+    </Wrapper>
   );
 }
 
