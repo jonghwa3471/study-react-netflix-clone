854b7258c5367552dfdd4cb4845533f950d753b3
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-07-23 19:10:19 +0900

    #8.2 Basic Animations

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,24 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
-      <motion.div></motion.div>
+      <Box
+        transition={{ type: "spring", delay: 0.5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      >
+        motion
+      </Box>
     </Wrapper>
   );
 }
