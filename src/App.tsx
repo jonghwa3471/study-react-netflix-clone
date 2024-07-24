eed9fe4ea75d4d1928c361c2beeae9377d07cbdd
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-07-24 19:46:56 +0900

    #8.5 Gestures part One

--- src/App.tsx
+++ src/App.tsx
@@ -10,59 +10,32 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  place-self: center;
-  width: 70px;
-  height: 70px;
-  border-radius: 50%;
-  background-color: white;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants = {
-  start: { opacity: 0, scale: 0.5 },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.3,
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
+  hover: { scale: 1.5, rotateZ: 90 },
+  tap: { scale: 1, borderRadius: "50%" },
+  drag: {
+    backgroundColor: "rgba(46, 204, 113,1.0)",
+    transition: { duration: 10 },
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box
+        drag
+        variants={boxVariants}
+        whileHover="hover"
+        whileTap="tap"
+        whileDrag="drag"
+      />
     </Wrapper>
   );
 }
