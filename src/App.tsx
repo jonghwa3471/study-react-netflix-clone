cc5e0398b15a272a5aa0732f7a2f9c1339ffc50e
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-09-16 01:26:39 +0900

    add hashRouter and homepageUrl

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Routes/Home";
 import Tv from "./Routes/Tv";
 import Search from "./Routes/Search";
